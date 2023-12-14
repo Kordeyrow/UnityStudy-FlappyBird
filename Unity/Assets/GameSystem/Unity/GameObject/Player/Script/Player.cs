4885ddeb959d462fd06fa68b50ceabddcddecbc6
Kordeyrow <matheusgoncalvescordeiro@gmail.com>
2023-12-14 18:53:33 -0300

    (-) Deleting custom containers

--- Unity/Assets/GameSystem/Unity/GameObject/Player/Script/Player.cs
+++ Unity/Assets/GameSystem/Unity/GameObject/Player/Script/Player.cs
@@ -4,11 +4,6 @@ using UnityLayer;
 
 public class Player : MonoBehaviour, IPlayer
 {
-    [SerializeField]
-    [RequireInterface(typeof(IPlayerContainer))]
-    UnityEngine.Object playerContainerObj;
-    IPlayerContainer playerContainer;
-    IPlayer thisPlayer;
     [SerializeField] Rigidbody2D rigidbody2D;
     [SerializeField] SpriteRenderer spriteRenderer;
     [SerializeField] Collider2D collider2D;
@@ -20,21 +15,18 @@ public class Player : MonoBehaviour, IPlayer
     private void Awake()
     {
         jump2D = GetComponent<IJump2D>();
-        thisPlayer = this;
-        playerContainer = playerContainerObj as IPlayerContainer;
     }
 
     void Start()
     {
         jump2D?.Execute();
-        playerContainer?.SetPlayer(thisPlayer);
     }
 
     void IPlayer.Die()
     {
-        OnDie?.Invoke();
         StopMovement();
         Freeze();
+        OnDie?.Invoke();
     }
 
     void StopMovement()
--- Unity/Assets/Scene/GameScene/GameSceneBuilder.cs
+++ Unity/Assets/Scene/GameScene/GameSceneBuilder.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using Zenject;
 
-public class MainSceneBuilder : MonoInstaller
+public class GameSceneBuilder : MonoInstaller
 {
     [Header("> GameObjects")]
     [RequireInterface(typeof(IPlayer))]
